4995e27e354b1e9ada379a6c6fb925f1dbb9b785
Chen Ping <maple-94@hotmail.com>
2024-11-03 17:13:39 +0900

    created venv

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\chenp\Dev\own\win4r-o1\COTlike-llama\venv
+set VIRTUAL_ENV=C:\Users\kim\dev\own\COTlike llama\COTlike-llama\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
